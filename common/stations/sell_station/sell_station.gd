221acdf657bd46b3fdbbfc4ca31426131f307541
BurgessDunneD8 <150651732+BurgessDunneD8@users.noreply.github.com>
2026-01-28 03:30:45 -0500

    Make mineral count text white after selling bag

--- common/stations/sell_station/sell_station.gd
+++ common/stations/sell_station/sell_station.gd
@@ -4,4 +4,5 @@ extends Node
 func interact(player: Player) -> void:
 	player.money_and_upgrades.add_money(player.mineral_inventory.sum_mineral_sell_values())
 	player.mineral_inventory.sell_all_minerals()
+	player.mineral_inventory.change_label_colour(Color.WHITE)
 	AudioManager.play_mineral_sell_sound()
