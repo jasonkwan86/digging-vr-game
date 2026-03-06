e86a848f6e37217b1e00501a5d3574e6a77798de
Jason Kwan <kwan.jason2001@gmail.com>
2026-03-05 23:32:22 -0500

    add headlamp

---
+++ common/player/headlamp/headlamp.gd
@@ -0,0 +1,12 @@
+class_name Headlamp
+extends Node3D
+
+@export var camera: Node3D
+@export var lerp_speed: float = 4
+
+@onready var spotlight = $SpotLight3D
+
+func _physics_process(delta: float) -> void:
+	spotlight.global_position = camera.global_position
+	var target_basis = camera.global_transform.basis.orthonormalized()
+	spotlight.global_transform.basis = spotlight.global_transform.basis.orthonormalized().slerp(target_basis, lerp_speed * delta)
